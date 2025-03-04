d862c4aabae362f30af6efb348c1094ace2ac22c
stevenazi14 <stevenaziavula14@gmail.com>
2025-03-04 08:41:07 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return """
+    <html>
+    <head>
+        <title>Welcome</title>
+        <style>
+            body {
+                background-color: #121212;
+                color: white;
+                font-family: Arial, sans-serif;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                text-align: center;
+            }
+            h1 {
+                font-size: 3em;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello, This is STEVEN AZIAVULA!</h1>
+    </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
